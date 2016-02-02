add5430574b09930c738fd75adf6219e8a90cda5
Paulo Silva <paulo.fernando.silva@gmail.com>
2016-02-02 16:43:00 +0900

    Added test for the existence of macdeployqt.

--- bin/osx-bundle.sh
+++ bin/osx-bundle.sh
@@ -2,20 +2,27 @@
 
 : ${1:?"bundle name must be given."}
 bundle="$1"
+: ${2:?"macdeployqt must be given"}
+deploy="$2"
 
-# I would prefer not to use the version#, but I have more versions installed on my system
-macdeployqt-4.8 $bundle
+${deploy} "${bundle}"
 
 binary="`basename $bundle .app`"
 contents="$bundle/Contents"
 fw="Frameworks"
 libdir="$contents/$fw"
 
+if [ ! -d "${libdir}" ]; then
+    echo "${libdir} doesn't exist!"
+    exit 1
+fi
+
 # I'm using the Magick++ installed through macports included in the ImageMagick port. Replace directory as needed.
-src_cdr="/opt/local/lib/ImageMagick-6.8.0/modules-Q16/coders"
+VER="`Magick++-config --version | awk '{print $1}'`"
+src_cdr="/opt/local/lib/ImageMagick-${VER}/modules-Q16/coders"
 
 if [ -d "$src_cdr" ]; then
-    cp -R $src_cdr/{gif,mpeg,jpeg}.so "$libdir"
+    cp -R ${src_cdr}/{gif,mpeg,jpeg}.so "$libdir"
     tag=' (compatibility '
 
     for so in $libdir/{gif,mpeg,jpeg}.so; do
@@ -33,9 +40,9 @@ else
     echo "$src not found. Edit this script and change the directory to match where you have the ImageMagick coders installed."
 fi
 
-if [ ! -f "$libdir/libclparser.dylib" ]; then 
-    cp /System/Library/Frameworks/OpenCL.framework/Versions/A/Libraries/libclparser.dylib "$libdir"
-fi
+# if [ ! -f "$libdir/libclparser.dylib" ]; then 
+#     cp /System/Library/Frameworks/OpenCL.framework/Versions/A/Libraries/libclparser.dylib "$libdir"
+# fi
 
 script_name="$binary"
 bin="$binary.app"
